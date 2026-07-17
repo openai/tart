057646cf8953121547573e5eb209dea2c88929ee
Fedor Kororkov <fkorotkov@openai.com>
2026-07-17 14:47:15 -0400

    Fix Tart release code signing (#1284)

--- .ci/sign-release.sh
+++ .ci/sign-release.sh
@@ -10,5 +10,15 @@ if [ "${TART_RELEASE_SNAPSHOT:-false}" = "true" ]; then
     --entitlements Resources/tart-dev.entitlements \
     dist/tart_darwin_all/tart.app
 else
-  gon gon.hcl
+  codesign \
+    --force \
+    --verbose \
+    --sign "Developer ID Application: Cirrus Labs, Inc. (9M2P8L4D89)" \
+    --timestamp \
+    --options runtime \
+    --keychain "$RUNNER_TEMP/build.keychain" \
+    --entitlements Resources/tart-prod.entitlements \
+    dist/tart_darwin_all/tart.app
+
+  codesign --verify --strict --verbose=2 dist/tart_darwin_all/tart.app
 fi
